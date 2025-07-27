879ff82b3ffd391527d769245c037f4053c441d4
bohyun-shin <bohyun.shin@linecorp.com>
2025-07-27 17:10:17 +0900

    feat: integrate fastgcn into original training pipeline

--- gnn/libs/parse_args.py
+++ gnn/libs/parse_args.py
@@ -5,13 +5,23 @@ def parse_args() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_name", type=str, required=True, choices=["cora"])
     parser.add_argument(
-        "--model_name", type=str, required=True, choices=["gcn", "graphsage", "gat"]
+        "--model_name",
+        type=str,
+        required=True,
+        choices=["gcn", "graphsage", "gat", "fastgcn"],
     )
     parser.add_argument(
         "--sage_aggregator", type=str, default="mean", choices=["mean", "max", "lstm"]
     )
     parser.add_argument("--num_heads", type=int, default=3)
     parser.add_argument("--leaky_relu_alpha", type=float, default=0.2)
+    parser.add_argument("--fastgcn_sample_size", type=int, default=5)
+    parser.add_argument(
+        "--fastgcn_sampling_method",
+        type=str,
+        default="importance",
+        choices=["importance", "uniform"],
+    )
     parser.add_argument("--learning_rate", type=float, default=0.01)
     parser.add_argument("--weight_decay", type=float, default=5e-4)
     parser.add_argument("--dropout", type=float, default=0.5)
